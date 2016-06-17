3fb9d92b5b23191b8dbb388eebea6ef96e53fd73
Joschka Groß <s8jagros@stud.uni-saarland.de>
2016-06-17 16:05:47 +0200

    Dividierer implementiert

--- aufwaermteil/DivisionsSchaltwerk.v
+++ aufwaermteil/DivisionsSchaltwerk.v
@@ -7,7 +7,50 @@ module Division(
 	output [31:0] r
 );
 
+	reg [31:0] divider;
+	reg [31:0] remainder;
+	integer takt;	
+	
 	// TODO Implementierung
+	
+	always @(start)
+	begin
+		divider <= 0;
+		remainder <= 0;
+		takt <= 31;
+	end
+	
+	always @(posedge clock)
+	begin
+		if (~start)
+		begin
+		
+			if (takt >= 0)
+			begin
+			
+				remainder = remainder << 1;
+				remainder = remainder + a[takt];
+				
+				if (remainder < b)
+				begin
+					divider[takt] <= 0;
+				end
+				
+				else
+				begin
+					divider[takt] = 1;
+					remainder <= remainder - b;
+				end
+				
+				takt = takt - 1;
+				
+			end
+			
+		end
+	end
+	
+	assign q = divider;
+	assign r = remainder;
 
 endmodule
 
