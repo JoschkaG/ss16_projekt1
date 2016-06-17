d962b090c382692285bd8faea0c9fc569764a7e6
Joschka Groß <s8jagros@stud.uni-saarland.de>
2016-06-17 16:25:22 +0200

    Mealy Testbench

--- aufwaermteil/MealyMaschine.v
+++ aufwaermteil/MealyMaschine.v
@@ -1,35 +1,55 @@
 module MealyPattern(
 	input        clock,
 	input        i,
-	output [1:0] o
+	output  [1:0] o
 );
 
 reg p,q;
+
 
 initial begin
 	p = 0;
 	q = 0; 
 end
+
+
 		always @(posedge clock) begin
 			p <= i;
 			q <= p;
 		end
-	assign o[0] = i & ~p & q;
-	assign o[1] = ~i & p & ~q;
-
+		
+	assign o[1] = i & ~p & q;
+	assign o[0] = ~i & p & ~q;
+	
 endmodule
 
 module MealyPatternTestbench();
 
-	reg clk,res;
+	reg clk,in;
 
 	initial begin
 		$dumpfile("Mealy.vcd");
 		$dumpvars;
-		res <= 1'b1;
-		#5;
-		res <= 1'b0;
-		#25;
+		in <= 1'b0;
+		#4
+		in <= 1'b1;
+		#4
+		in <= 1'b1;
+		#4
+		in <= 1'b0;
+		#4
+		in <= 1'b1;
+		#4
+		in <= 1'b0;
+		#4
+		in <= 1'b1;
+		#4
+		in <= 1'b0;
+		#4
+		in <= 1'b1;
+		#4
+		in <= 1'b1;
+		#4
 		$finish;
 	end
 
@@ -41,7 +61,7 @@ module MealyPatternTestbench();
 	wire [1:0] o;
 	wire i;
 
-	MealyPattern machine(.clock(clk), .i(i), .o(o));
+	MealyPattern machine(.clock(clk), .i(in), .o(o));
 
 	reg [0:8] e;
 	reg [0:8] s [1:0];
@@ -49,7 +69,6 @@ module MealyPatternTestbench();
 
 	initial begin
 		r = {9'b101001101};
-		s = {9'b001000001,9'b000100000};
 	end
 
 endmodule
